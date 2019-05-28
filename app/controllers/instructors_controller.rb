d0992ce76a5eaab0150f35bd9c095ef90889161f
github username <github email address>
2019-05-28 19:22:15 -0400

    completed instructor controller and routes

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,22 +1,44 @@
 class InstructorsController < ApplicationController
+
+  before_action :find_instructor, only: [:show, :edit, :update]
+
   def index
+    @instructors = Instructor.all
   end
 
   def new
-
+    @instructor = Instructor.new
   end
 
   def create
-
-  end
-
-  def show
-  end
-
-  def edit
-
+    @instructor = Instructor.new(instructor_params)
+    if @instructor.valid?
+      @instructor.save
+      redirect_to @instructor
+    else
+      flash[:error] = @instructor.error.full_messages
+      render :new
+    end
   end
 
   def update
+    @instructor = Instructor.update(instructor_params)
+    if @instructor.valid?
+      @instructor.save
+      redirect_to @instructor
+    else
+      flash[:error] = @instructor.error.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def instructor_params
+    params.require(:instructor).permit(:name, :mod, :mod_id)
+  end
+
+  def find_instructor
+    @instructor = Instructor.find(params[:id])
   end
 end
