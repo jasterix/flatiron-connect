e959ac1e3689f2617354a7d0e8077bcef70668c5
Jasterix <ljcalixte@gmail.com>
2019-05-28 19:29:39 -0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,99 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Student.destroy_all
+Instructor.destroy_all
+Note.destroy_all
+Resource.destroy_all
+Follow.destroy_all
+
+require 'faker'
+
+#MODS-------------------------------------------------------------------
+# mods = [1, 2, 3, 4, 5]
+# mods.map do |x|
+#   Mod.create(mod_number:x, mod_description: "Mod #{x}")
+# end
+
+#FOLLOWS-------------------------------------------------------------------
+# Follow.create(student: , resource: )
+#
+# student.resources <<
+
+#STUDENT-------------------------------------------------------------------
+
+student_ids = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+#
+student_names = []
+10.times do
+  student_names << Faker::Name.name
+end
+
+student_names.map do |student_name|
+  Student.create(name: student_name)
+end
+
+
+
+
+#INSTRUCTOR-----------------------------------------------------------------
+
+instructors = ["Kevin", "Graham"]
+tcfs = ["Helen", "Dan"]
+
+instructors.map do |instructor|
+  Instructor.create(name:instructor, mod_id: 1)
+end
+
+tcfs.map do |tcf|
+  Instructor.create(name:tcf, mod_id: 2)
+end
+
+
+#NOTES-------------------------------------------------------------------
+
+mod_1_lectures = ['Hashketball Review',
+'Hashes and the Internet',
+'Intro to Object-Oriented Programming',
+'Object Relationships: One to Many',
+'Object Relationships — Many to Many',
+'SQL Review',
+'Intro to ActiveRecord',
+'ActiveRecord Associations',
+'Rack and the Internet',
+'Intro to Sinatra and MVC',
+'Forms',
+'Sinatra REST I (CR)',
+'Sinatra REST II (UD)',
+'Sinatra Relationships']
+
+mod_2_lectures = ['Intro to Rails',
+'Rails Forms',
+'Rails Validations',
+'Sessions, Cookies, & Flash',
+'Authorization',
+'Authentication']
+
+mod_3_lectures = ['Intro to Front-End Web Development',
+'Intro to the DOM',
+'Intro to Events',
+'DOM CRUD']
+
+
+
+mod_1_lectures.map do |lecture|
+  Note.create!(lecture_name: lecture, student_id: Student.first.id, mod_id:1)
+end
+
+# mod_2_lectures.map do |lecture|
+#   Note.create(lecture_name: lecture, student_id: student_ids.sample, mod_id:1)
+# end
+#
+# mod_3_lectures.map do |lecture|
+#   Note.create(lecture_name: lecture, student_id: student_ids.sample, mod_id:1)
+# end
