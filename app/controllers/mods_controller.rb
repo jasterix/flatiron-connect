48d041cf37c0563291b6ff10e01bb5a937b53b07
Jasterix <ljcalixte@gmail.com>
2019-05-29 09:41:31 -0400

    recommitting mod and notes pages

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -1,11 +1,15 @@
 class ModsController < ApplicationController
+  before_action :get_mod, only: [:show, :edit, :update, :destroy]
+
   def index
     @mods = Mod.all
   end
 
   def show
-
-
   end
 
+
+  def get_mod
+  @mod = Mod.find(params[:id])
+end
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,12 +1,23 @@
 class NotesController < ApplicationController
+  before_action :get_note, only: [:show, :edit, :update, :destroy]
+
   def index
+    @note = Note.all
   end
 
   def new
-
+    @note = Note.create()
   end
 
   def create
+    @note = Note.create(note_params)
+    if @note.valid?
+      # @note.save
+      redirect_to @note
+    else
+      flash[:error] = @note.errors.full_messages
+      render 'new'
+    end
   end
 
   def show
@@ -20,5 +31,15 @@ class NotesController < ApplicationController
   end
 
   def delete
+    @note.delete
   end
+
+  private
+  def note_params
+    params.require(:note).permit(:lecture_name, :content, :highlights, :student_id, :mod_id)
+  end
+
+  def get_note
+   @note = Note.find(params[:id])
+ end
 end
